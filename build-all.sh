f7fd9027ed30bb2cf63226552689de728058e144
Alexandre Mclean <alexandre.mclean@gmail.com>
2015-07-13 21:11:04 -0400

    Add cinder

--- build-all.sh
+++ build-all.sh
@@ -3,3 +3,4 @@
 ./build.sh originals/nova/icehouse nova icehouse
 ./build.sh originals/keystone/icehouse keystone icehouse
 ./build.sh originals/glance/icehouse glance icehouse
+./build.sh originals/cinder/icehouse cinder icehouse
