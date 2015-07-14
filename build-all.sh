8ce8a726a35fdbf7cf5ee449065c3c7765372483
Mclean Alexandre <alexandre.mclean@ubisoft.com>
2015-07-14 16:29:04 -0400

    Add nova juno

--- build-all.sh
+++ build-all.sh
@@ -4,3 +4,5 @@
 ./build.sh originals/keystone/icehouse keystone icehouse
 ./build.sh originals/glance/icehouse glance icehouse
 ./build.sh originals/cinder/icehouse cinder icehouse
+
+./build.sh originals/nova/juno nova juno
